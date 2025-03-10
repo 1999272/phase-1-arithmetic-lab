932bc2bcce7023f59ff16e11d245bd6e5802a0e7
Mark_Kamau <mark.wainaina@student.moringaschool.com>
2025-03-10 15:36:40 +0300

    Adds additional Data

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// Multiply two numbers to get 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; 
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random()*100) + 1;
+
+// Calculate a remainder of 4
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Find the maximum number in a set
+const max = Math.max(1,5,10,15,20);
